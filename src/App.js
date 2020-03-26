9bf851b6fe92a414d983ac891c302a4d7ed5307d
Fernando Espinosa <fer@ferdev.com>
2020-03-26 19:24:51 +0100

    Add @wordpress/components modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Modal } from '@wordpress/components';
+
+function __( text ) {
+  return text;
+}
+
+function onClose() {
+
+}
+
+function remindMeLaterClicked() {
+
+}
+
+function closeForeverClicked() {
+
+}
 
 function App() {
   return (
@@ -18,6 +35,27 @@ function App() {
         >
           Learn React
         </a>
+
+        <Modal
+          title={ __( 'Are you sure?', 'woocommerce-admin' ) }
+          onRequestClose={ onClose }
+          className="wc-admin-shipping-banner__dismiss-modal"
+        >
+          <p className="wc-admin-shipping-banner__dismiss-modal-help-text">
+            { __(
+              'With WooCommerce Shipping you can Print shipping labels from your WooCommerce dashboard at the lowest USPS rates.',
+              'woocommerce-admin'
+            ) }
+          </p>
+          <div className="wc-admin-shipping-banner__dismiss-modal-actions">
+            <Button isDefault onClick={ remindMeLaterClicked }>
+              { __( 'Remind me later', 'woocommerce-admin' ) }
+            </Button>
+            <Button isPrimary onClick={ closeForeverClicked }>
+              { __( "I don't need this", 'woocommerce-admin' ) }
+            </Button>
+          </div>
+        </Modal>
       </header>
     </div>
   );
